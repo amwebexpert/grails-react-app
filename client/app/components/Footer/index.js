776495ddca1f0a34c286875ff9727e23c5c505fb
amwebexpert <amwebexpert@gmail.com>
2020-02-06 16:46:08 -0500

    Made AdminLTE Footer

--- client/app/components/Footer/index.js
+++ client/app/components/Footer/index.js
@@ -1,29 +1,17 @@
+import LocaleToggle from 'containers/LocaleToggle';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-
-import A from 'components/A';
-import LocaleToggle from 'containers/LocaleToggle';
-import Wrapper from './Wrapper';
 import messages from './messages';
+
 
 function Footer() {
   return (
-    <Wrapper>
-      <section>
-        <FormattedMessage {...messages.licenseMessage} />
-      </section>
-      <section>
+    <footer className="main-footer">
+      <FormattedMessage {...messages.licenseMessage} />
+      <div className="float-right d-none d-sm-inline-block">
         <LocaleToggle />
-      </section>
-      <section>
-        <FormattedMessage
-          {...messages.authorMessage}
-          values={{
-            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-          }}
-        />
-      </section>
-    </Wrapper>
+      </div>
+    </footer>
   );
 }
 
