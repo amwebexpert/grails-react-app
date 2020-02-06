793800ec2e1ec848f8b588ddbe4af8edade9868a
amwebexpert <amwebexpert@gmail.com>
2020-02-06 16:07:41 -0500

    Associated navigation bar to Header component

--- client/app/components/Header/index.js
+++ client/app/components/Header/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import HeaderLink from './HeaderLink';
+import { Link } from 'react-router-dom';
 import messages from './messages';
-
 
 function Header() {
   return (
-    <div>
-      <HeaderLink to="/">
-        <FormattedMessage {...messages.home} />
-      </HeaderLink>
-      <HeaderLink to="/features">
-        <FormattedMessage {...messages.features} />
-      </HeaderLink>
-    </div>
+    <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" data-widget="pushmenu" href="#"><i className="fas fa-bars"></i></a>
+        </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          <Link to="/" className="nav-link"><FormattedMessage {...messages.home} /></Link>
+        </li>
+        <li className="nav-item d-none d-sm-inline-block">
+          <Link to="/features" className="nav-link"><FormattedMessage {...messages.features} /></Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
 export default Header;
+
