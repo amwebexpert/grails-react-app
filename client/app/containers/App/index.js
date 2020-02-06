b19f623798af9a229897392643bcc4faf868378a
amwebexpert <amwebexpert@gmail.com>
2020-02-06 16:36:48 -0500

    Removed useless global styles

--- client/app/containers/App/index.js
+++ client/app/containers/App/index.js
@@ -6,46 +6,46 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import FeaturePage from 'containers/FeaturePage/Loadable';
+import HomePage from 'containers/HomePage/Loadable';
+import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
-import { Switch, Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import Header from 'components/Header';
-import Footer from 'components/Footer';
-
-import GlobalStyle from '../../global-styles';
-
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
 
 export default function App() {
   return (
-    <AppWrapper>
+    <div>
       <Helmet titleTemplate="%s - Cloud Files Manager" defaultTitle="Cloud Files Manager">
         <meta name="description" content="Cloud Files Manager application" />
       </Helmet>
 
       <Header />
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
+      <div class="content-wrapper">
+
+        <div class="content-header">
+          <div class="container-fluid">
+            <p>Page title ...</p>
+          </div>
+        </div>
+
+        <section class="content">
+          <div class="container-fluid">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/features" component={FeaturePage} />
+              <Route path="" component={NotFoundPage} />
+            </Switch>
+          </div>
+        </section>
+      </div>
 
       <Footer />
 
-      <GlobalStyle />
-    </AppWrapper>
+    </div>
   );
 }
